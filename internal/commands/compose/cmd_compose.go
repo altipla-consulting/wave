3b80fc6852346e574cfc60d36a957b05849fd2fe
Ernesto Alejo <ernestokarim@gmail.com>
2023-01-29 20:23:30 +0100

    Clean up the known hosts of the IPs too before connecting to the remote machine.

--- internal/commands/compose/cmd_compose.go
+++ internal/commands/compose/cmd_compose.go
@@ -1,6 +1,8 @@
 package compose
 
 import (
+	"context"
+	"net"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -25,9 +27,8 @@ func init() {
 	Cmd.Flags().StringVar(&flagSentry, "sentry", "", "Name of the sentry project to configure.")
 	Cmd.MarkPersistentFlagRequired("sentry")
 
-	Cmd.RunE = func(command *cobra.Command, args []string) error {
+	Cmd.RunE = func(cmd *cobra.Command, args []string) error {
 		logger := log.WithField("machine", args[0])
-
 		logger.WithField("version", query.Version()).Info("Deploy to remote machine with Docker Compose")
 
 		client, err := sentry.NewClient(os.Getenv("SENTRY_AUTH_TOKEN"), nil, nil)
@@ -43,20 +44,15 @@ func init() {
 			return errors.Trace(err)
 		}
 
-		keygen := exec.Command("ssh-keygen", "-F", args[0])
-		keygen.Stderr = os.Stderr
-		if err := keygen.Run(); err != nil {
-			if exit := new(exec.ExitError); !errors.As(err, &exit) || exit.ExitCode() != 1 {
-				return errors.Trace(err)
-			}
-
-			// Error is expected, the host is not in the known hosts.
-		} else {
-			// Remove the stored host, could be outdated.
-			logger.Info("Removing old stored host authentication")
-			rm := exec.Command("ssh-keygen", "-R", args[0])
-			rm.Stderr = os.Stderr
-			if err := rm.Run(); err != nil {
+		if err := cleanHost(cmd.Context(), logger, args[0]); err != nil {
+			return errors.Trace(err)
+		}
+		ips, err := net.DefaultResolver.LookupHost(cmd.Context(), args[0])
+		if err != nil {
+			return errors.Trace(err)
+		}
+		for _, ip := range ips {
+			if err := cleanHost(cmd.Context(), logger, ip); err != nil {
 				return errors.Trace(err)
 			}
 		}
@@ -71,7 +67,7 @@ func init() {
 			return errors.Trace(err)
 		}
 		defer f.Close()
-		keyscan := exec.Command("ssh-keyscan", args[0])
+		keyscan := exec.CommandContext(cmd.Context(), "ssh-keyscan", args[0])
 		keyscan.Stdout = f
 		keyscan.Stderr = os.Stderr
 		if err := keyscan.Run(); err != nil {
@@ -79,14 +75,14 @@ func init() {
 		}
 
 		logger.Info("Building remote containers")
-		build := exec.Command("docker", "compose", "-f", "docker-compose.prod.yml", "build")
+		build := exec.CommandContext(cmd.Context(), "docker", "compose", "-f", "docker-compose.prod.yml", "build")
 		prepareComposeCommand(build, args[0], keys[0].DSN.Public)
 		if err := build.Run(); err != nil {
 			return errors.Trace(err)
 		}
 
 		logger.Info("Sending container changes to the remote machine")
-		up := exec.Command("docker", "compose", "-f", "docker-compose.prod.yml", "up", "-d")
+		up := exec.CommandContext(cmd.Context(), "docker", "compose", "-f", "docker-compose.prod.yml", "up", "-d")
 		prepareComposeCommand(up, args[0], keys[0].DSN.Public)
 		if err := up.Run(); err != nil {
 			return errors.Trace(err)
@@ -107,4 +103,27 @@ func prepareComposeCommand(cmd *exec.Cmd, machine, sentryDSN string) {
 
 func apiString(s string) *string {
 	return &s
+}
+
+func cleanHost(ctx context.Context, logger *log.Entry, host string) error {
+	keygen := exec.CommandContext(ctx, "ssh-keygen", "-F", host)
+	keygen.Stderr = os.Stderr
+	if err := keygen.Run(); err != nil {
+		if exit := new(exec.ExitError); !errors.As(err, &exit) || exit.ExitCode() != 1 {
+			return errors.Trace(err)
+		}
+
+		// Error is expected, the host is not in the known hosts.
+		return nil
+	} else {
+		// Remove the stored host, could be outdated.
+		logger.WithField("host", host).Info("Removing old stored host authentication")
+		rm := exec.Command("ssh-keygen", "-R", host)
+		rm.Stderr = os.Stderr
+		if err := rm.Run(); err != nil {
+			return errors.Trace(err)
+		}
+	}
+
+	return nil
 }
