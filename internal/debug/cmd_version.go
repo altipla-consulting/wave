4c925ba28303f165256555ffa141b97e5b99842d
Andres Alejo <andresal2004@gmail.com>
2023-10-14 20:51:55 +0200

    Testea que se coja la version correcta

--- internal/debug/cmd_version.go
+++ internal/debug/cmd_version.go
@@ -9,15 +9,14 @@ import (
 
 var cmdVersion = &cobra.Command{
 	Use:     "version",
-	Short:   "Debug the version that is taken from a project.",
-	Example: "wave debug version example",
+	Short:   "Debug the deployment version.",
+	Example: "wave debug version",
 	Args:    cobra.ExactArgs(0),
 }
 
 func init() {
 	cmdVersion.RunE = func(cmd *cobra.Command, args []string) error {
-		version := query.Version()
-		fmt.Println(version)
+		fmt.Println(query.Version())
 		return nil
 	}
 }
